722024227caf7f1ecbf5fb0acdebb8749d7ab6e7
unknown <munyaneza@TW048.transvalor.com>
2016-04-13 11:37:28 +0200

    add node app

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var server  = express();
+
+
+var port = process.env.PORT || 5000;
+
+server.get('/', function(req, res){
+    res.send('Hello Jenkins');
+});
+
+server.listen(port, function(){
+    console.log('server running on http://localhost:%s', port);
+});
+
+module.exports = server;
