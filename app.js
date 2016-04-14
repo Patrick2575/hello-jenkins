28978ce1c435850c7e0fe45d5cd4446ff3288f92
Patrick2575 <munya_patrick23@yahoo.fr>
2016-04-14 09:11:43 +0200

    remove ! from msg

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var server  = express();
 var port = process.env.PORT || 5000;
 
 server.get('/', function(req, res){
-    res.send('Hello Jenkins!');
+    res.send('Hello Jenkins');
 });
 
 server.listen(port, function(){
